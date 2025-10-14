352d75e0df297cf929454183fb5e1e72acf879fa
AgusNoriega <agusnori88@gmail.com>
2025-10-13 22:14:09 -0300

    Update ejercicio2

--- Desafio 9/src/ejercicio3.java
+++ Desafio 9/src/ejercicio3.java
@@ -7,32 +7,15 @@
  *
  * @author agusn
  */
-public class Ejercicio2 extends javax.swing.JFrame {
+public class ejercicio3 extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Ejercicio2.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ejercicio3.class.getName());
 
     /**
-     * Creates new form Ejercicio2
+     * Creates new form ejercicio3
      */
-    public Ejercicio2() {
+    public ejercicio3() {
         initComponents();
-         // Agregar los tamaños al ComboBox
-   
-    cbNumeros.setModel(new javax.swing.DefaultComboBoxModel<>(
-    new String[] {"12","14","16","18","20"}
-));
-cbNumeros.setSelectedItem("12"); // si querés valor inicial
-
-
-    // Tamaño inicial del texto
-    lblTexto.setFont(new java.awt.Font("Arial", java.awt.Font.PLAIN, 12));
-
-    // Escuchar cuando el usuario selecciona un tamaño
-   cbNumeros.addActionListener(e -> {
-    int nuevo = Integer.parseInt(cbNumeros.getSelectedItem().toString());
-    lblTexto.setFont(lblTexto.getFont().deriveFont((float) nuevo));
-});
-
     }
 
     /**
@@ -44,32 +27,17 @@ cbNumeros.setSelectedItem("12"); // si querés valor inicial
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jPanel1 = new javax.swing.JPanel();
-        cbNumeros = new javax.swing.JComboBox<>();
-        lblTexto = new javax.swing.JLabel();
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        cbNumeros.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Numero" }));
-        jPanel1.add(cbNumeros, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
-
-        lblTexto.setText("Texto de Ejemplo");
-        jPanel1.add(lblTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 30, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -97,13 +65,9 @@ cbNumeros.setSelectedItem("12"); // si querés valor inicial
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new Ejercicio2().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new ejercicio3().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> cbNumeros;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel lblTexto;
     // End of variables declaration//GEN-END:variables
 }
