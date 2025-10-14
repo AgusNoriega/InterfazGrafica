218ffe065bb30f366d768a7ed1bde6d07fd78a33
AgusNoriega <agusnori88@gmail.com>
2025-10-13 22:34:19 -0300

    Update ejercicio 3

--- Desafio 9/src/ejercicio3.java
+++ Desafio 9/src/ejercicio3.java
@@ -2,6 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
 /**
  *
@@ -10,14 +14,48 @@
 public class ejercicio3 extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ejercicio3.class.getName());
+    
+  private final Map<String,String> info = new HashMap<>();
+private final DefaultListModel<String> modelo = new DefaultListModel<>();
 
-    /**
-     * Creates new form ejercicio3
-     */
-    public ejercicio3() {
-        initComponents();
-    }
 
+   public ejercicio3() {
+    initComponents();
+    setTitle("Lenguajes de Programación");
+    setLocationRelativeTo(null);
+
+    // modelo + datos
+    DefaultListModel<String> modelo = new DefaultListModel<>();
+    String[] lenguajes = {"Java","Python","C++","JavaScript","C#","Go","Ruby","Kotlin","Swift","PHP"};
+    for (String l : lenguajes) modelo.addElement(l);
+    jList1.setModel(modelo);
+    jList1.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+    // mapa de info (puede ser un campo de instancia)
+    java.util.Map<String,String> info = new java.util.HashMap<>();
+    info.put("Java","Java: orientado a objetos, JVM, backend, Android.");
+    // ... resto
+
+    // doble clic
+    jList1.addMouseListener(new java.awt.event.MouseAdapter() {
+        @Override public void mouseClicked(java.awt.event.MouseEvent e) {
+            if (e.getClickCount() == 2) {
+                int i = jList1.locationToIndex(e.getPoint());
+                if (i >= 0) {
+                    String lang = jList1.getModel().getElementAt(i);
+                    String msg = info.getOrDefault(lang, "Sin información disponible.");
+                    javax.swing.JOptionPane.showMessageDialog(ejercicio3.this, msg, lang,
+                            javax.swing.JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        }
+    });
+}
+}
+
+    
+     
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,17 +65,36 @@ public class ejercicio3 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        lstNombres = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        lstNombres.setViewportView(jList1);
+
+        jPanel1.add(lstNombres, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -47,27 +104,15 @@ public class ejercicio3 extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new ejercicio3().setVisible(true));
+            java.awt.EventQueue.invokeLater(() -> {
+        ejercicio3 frame = new ejercicio3();
+        frame.setVisible(true);
+    });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane lstNombres;
     // End of variables declaration//GEN-END:variables
-}
+
